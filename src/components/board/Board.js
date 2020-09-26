9d4e2df33eda1c98e00c95451e0009ccb267e91f
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-09-26 13:14:45 +0200

    Make Ghosts move

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -6,9 +6,15 @@ class Board extends Component {
     render() {
         return (
             <div className="board">
+                {/* FOOD */}
                 {/* <Food /> */}
+                {/* PACMAN */}
                 <Pacman />
+                {/* GHOSTS */}
                 <Ghost color={'blue'} />
+                <Ghost color={'pink'} />
+                <Ghost color={'red'} />
+                <Ghost color={'yellow'} />
             </div>
         )
     }
