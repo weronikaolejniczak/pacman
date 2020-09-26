5a76f544877cf975056fd5c228b105cc997c79bf
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-09-26 17:37:33 +0200

    Extend To Do list

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import './App.css';
 - Ghost and Pacman collision, collision animation and game over
 - Add Walls
 - Add Pacman lives
-- Start Game, Game Over and Success screens */
+- Start Game, Game Over and Success screens 
+- HOC Sprint for Pacman and Ghost and render prop
+- Always focus on Pacman (onClick App?)
+*/
 function App() {
   const [score, setScore] = useState(0);
 
