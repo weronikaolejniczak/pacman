e9baeb61d51d252da98b64e847516622e568ebac
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-09-26 12:41:55 +0200

    Ghost component

---
+++ src/components/ghost/Ghost.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {ReactComponent as Icon} from './ghost.svg'
+import './style.css';
+
+class Ghost extends Component {
+    state = {
+        position: {
+            top: 50,
+            left: 50,
+        }
+    }
+
+    render() {
+        const {position} = this.state;
+
+        return(
+            <div 
+                className="ghost"
+                style={position}
+            >
+                <Icon />
+            </div>
+        );
+    }
+}
+
+Ghost.defaultProps = {
+    step: 50, // 50px
+    size: 50, // ghost size: 50x50px
+    // TODO: move to config
+    border: 10 * 2,
+    topScoreBoardHeight: 50,
+}
+
+export default Ghost;
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './header/Header';
 import Board from './board/Board';
 import Pacman from './pacman/Pacman';
+import Ghost from './ghost/Ghost';
 
-export {Header, Board, Pacman};
+export {Header, Board, Pacman, Ghost};
