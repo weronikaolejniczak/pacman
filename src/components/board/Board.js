58a1f4f05bf7ef514d3f827936d02ff1840ae6cf
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-09-26 13:50:32 +0200

    Render Food components on the Board

--- src/components/board/Board.js
+++ src/components/board/Board.js
@@ -2,12 +2,56 @@ import React, {Component} from 'react';
 import {Pacman, Ghost, Food} from '../';
 import './style.css';
 
+/* TODO: add observer pattern for viewport to calculate proper food for modified viewport */
 class Board extends Component {
+    constructor(props) {
+        super(props);
+
+        this.food = [];
+        // calculate the amount of food
+        this.amountOfFood = (
+            (window.innerWidth - this.props.foodSize) 
+            * (window.innerHeight - this.props.topScoreBoardHeight) / (this.props.foodSize * this.props.foodSize) - 1
+        )
+
+        for (let i = 0; i < this.amountOfFood; i++) {
+            this['food' + i] = React.createRef();
+        }
+    }
+
     render() {
+        const {foodSize, border, topScoreBoardHeight} = this.props;
+
+        let foods = [];
+        let currentTop = 0;
+        let currentLeft = 1 * foodSize;
+
+        for (let i = 0; i < this.amountOfFood; i++) {
+            // do we have to go to another line as to avoid going out of the board?
+            if (currentLeft + foodSize >= window.innerWidth - this.props.border) {
+                currentTop += foodSize;
+                currentLeft = 0;
+            }
+            // do we have to end rendering food (bottom right corner)?
+            if (currentTop + foodSize >= (window.innerHeight - border - topScoreBoardHeight)) {
+                break;
+            }
+
+            const position = {left: currentLeft, top: currentTop};
+            currentLeft += foodSize;
+            foods.push(
+                <Food
+                    key={`food-elem-${i}`}
+                    position={position}
+                    ref={this['food' + i]}
+                />
+            )
+        }
+
         return (
             <div className="board">
                 {/* FOOD */}
-                <Food position={{top: 50, left: 50}} />
+                {foods}
                 {/* PACMAN */}
                 <Pacman />
                 {/* GHOSTS */}
@@ -18,6 +62,13 @@ class Board extends Component {
             </div>
         )
     }
+}
+
+// TODO: refactor and move to config
+Board.defaultProps = {
+    foodSize: 50,
+    border: 10 * 2,
+    topScoreBoardHeight: 50
 }
 
 export default Board;
